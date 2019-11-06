67716c6268ef23f8fb94594f8374db951db7c181
GMike the GD <mikeolams@yahoo.com>
2019-11-06 09:12:28 +0100

    add chatbot

--- resources/views/contact_support.blade.php
+++ resources/views/contact_support.blade.php
@@ -437,5 +437,18 @@ a.article:hover {
         x.classList.toggle('active');
       }
     </script>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5dc11896e4c2fa4b6bda03bf/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
   </body>
   @endsection
