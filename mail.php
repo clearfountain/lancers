58daa2b926485b03eb6489700f3460568ede994b
e770r <cyberdev01@protonmail.com>
2019-11-06 19:36:36 +0100

    updated the script

--- mail.php
+++ mail.php
@@ -28,7 +28,7 @@ if(isset($_POST['email'])){
   $mail->Body    = 'mail body';
  // $mail->AltBody = '';
   if(!$mail->send()) {  
-      echo "<span style='font-size:15px; color:#FFFFFF; text-algin: center; background-color: red;'> Oops!! We were unable to send you a mail, please try again later.</span>";
+      echo "<span style='font-size:16px; color:#FFFFFF; text-algin: center; background-color: red;'> Oops!! We were unable to send you a mail, please try again later.</span>";
       //echo 'Mailer Error: ' . $mail->ErrorInfo . "n";
   } else {
       echo "<span style='font-size:15px; color:#FFFFFF;  text-algin: center; background-color: green;'>Email sent successfully</span>";
