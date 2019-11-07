ad021d39761d60c60871e86b3b83e3ce61d848e0
Silas Adedoyin <silasfaarda@gmail.com>
2019-11-07 15:42:44 +0100

    some changes

--- database/migrations/2019_10_09_224457_create_transactions_table.php
+++ database/migrations/2019_10_09_224457_create_transactions_table.php
@@ -17,20 +17,20 @@ class CreateTransactionsTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id')->unsigned()->index('user_id');
                 $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('subscription_id')->unsigned();
-                $table->foreign('subscription_id')->references('id')->on('subscriptions');
-            $table->decimal('amount', 20, 2)->comment('Amount of money that was credited or debited');
+            // $table->unsignedBigInteger('subscription_id')->unsigned();
+                // $table->foreign('subscription_id')->references('id')->on('subscriptions');
+            $table->decimal('amount', 20, 2);
             $table->text('narration')->nullable();
-            $table->string('country')->nullable();
-            $table->string('currency')->nullable();
+            // $table->string('country')->nullable();
+            // $table->string('currency')->nullable();
             $table->string('reference')->unique('reference')->nullable();
             $table->enum('status', ['pending', 'succesful', 'failed']);
-            $table->string('channel')->nullable()->comment('bank transfer, or instant credit/debit card, or a saved debit/credit card');
-            $table->string('ip_address')->nullable(); //use the correct ip address type
-            $table->string('site_referrer')->nullable();
-            $table->string('attempts')->nullable();
-            $table->string('fees')->nullable();
-            $table->text('comment')->nullable();
+            // $table->string('channel')->nullable()->comment('bank transfer, or instant credit/debit card, or a saved debit/credit card');
+            // $table->string('ip_address')->nullable(); //use the correct ip address type
+            // $table->string('site_referrer')->nullable();
+            // $table->string('attempts')->nullable();
+            // $table->string('fees')->nullable();
+            // $table->text('comment')->nullable();
             $table->timestamps();
         });
     }
